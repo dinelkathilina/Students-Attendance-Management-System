6c0f60e2bffc354af1f6a2ed3ce7dc8288bcb1c6
dinelkathilina <dinelkathilina3@gmail.com>
2024-09-14 23:05:23 +0530

    authservice imported

--- AMS/src/routes/Createsession.jsx
+++ AMS/src/routes/Createsession.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import QRCode from "qrcode";
-import authservice from "../services/authservice";
 import { useSession } from "../Context/SessionContext";
+import authservice from "../Services/authservice";
 
 export const Createsession = () => {
 
